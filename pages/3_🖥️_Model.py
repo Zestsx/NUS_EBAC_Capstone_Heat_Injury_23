d81d3aabcbfb9b06a59e25f7bd0cfbaee154bb36
Spencer <zestsx@gmail.com>
2023-08-25 17:36:18 +0800

    Update 3_🖥️_Model.py

--- pages/3_🖥️_Model.py
+++ pages/3_🖥️_Model.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 from PIL import Image
 # other libs
 
@@ -86,6 +87,159 @@ df['avr_temperature'].fillna(average_value_temp, inplace=True)
 average_value_humidity = df['avr_humidity'].mean()
 # Impute the average value for NaNs in the column
 df['avr_humidity'].fillna(average_value_humidity, inplace=True)
+
+#####################
+#Model
+
+variables_nonpca = [
+    'Age',
+    'Gender',
+    'Height (m)',
+    'Weight (Kg)',
+    'BMI',
+    'vo2_relative',
+    'vo2_absolute',
+    'resting_hr',
+    'bp_systolic_diastolic',
+    'body_fat_perc',
+    'avr_temperature',
+    'avr_humidity',
+    'predicted_BT',
+    'predicted_HR'
+]
+
+temp=df[variables_nonpca]
+
+# convert category(str) to numerical data
+temp['Gender'] = temp['Gender'].map({'M':0, 'F':1})
+
+# compute the vif for all given features
+def compute_vif(temp_df,considered_features):
+
+    X = temp_df[considered_features]
+    # the calculation of variance inflation requires a constant
+    X['intercept'] = 1
+
+    # create dataframe to store vif values
+    vif = pd.DataFrame()
+    vif["Variable"] = X.columns
+    vif["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+    vif = vif[vif['Variable']!='intercept']
+    return vif
+
+compute_vif(temp,variables_nonpca).sort_values('VIF', ascending=False)
+
+# drop value because it is >5
+variables_nonpca.remove('Weight (Kg)')
+
+compute_vif(temp,variables_nonpca).sort_values('VIF', ascending=False)
+
+# drop value because it is >5
+variables_nonpca.remove('vo2_absolute')
+
+# drop value because it is >5
+variables_nonpca.remove('Gender')
+
+compute_vif(df_cph,variables_cox_nonpca).sort_values('VIF', ascending=False)
+
+moreThan5=True
+while moreThan5:
+  val=compute_vif(df_cph,variables_cox_nonpca).sort_values('VIF', ascending=False).head(1)['VIF'].values[0]
+  if val>5:
+    column=compute_vif(df_cph,variables_cox_nonpca).sort_values('VIF', ascending=False).head(1)['Variable'].values[0]
+    print("drop column: {}, VIF: {}".format(column,val))
+    variables_cox_nonpca.remove(column)
+  else:
+    moreThan5=False
+
+df_cph=df_cph[variables_cox_nonpca]
+
+# split data into train and test sets
+train_data, test_data = train_test_split(df_cph, test_size=0.2, random_state=42)
+
+#TRAIN
+# create the proportional hazards model
+cph = CoxPHFitter(penalizer = 0.1)
+
+target = 'time_to_event'
+event = 'Heat Stroke'
+
+# fit the model on the training data, specifying model effects
+cph.fit(train_data, duration_col=target, event_col=event)
+
+# print the TRAIN coefficients with p-values
+
+# evaluate the model
+concordance_index = cph.score(train_data, scoring_method='concordance_index')
+print('Concordance index for TRAIN:', concordance_index)
+
+summary = cph.summary
+summary['Significant'] = ['Y' if p < 0.05 else 'N' for p in summary['p']]
+
+sorted_summary = summary.sort_values(by='exp(coef)', ascending=False)
+sorted_summary
+
+# TEST
+
+# fit the model on the test data, specifying model effects
+cph.fit(test_data, duration_col=target, event_col=event)
+
+# print TEST coefficients with p-values
+
+# evaluate the model
+concordance_index = cph.score(test_data, scoring_method='concordance_index')
+print('Concordance index for TEST:', concordance_index)
+
+summary = cph.summary
+summary['Significant'] = ['Y' if p < 0.05 else 'N' for p in summary['p']]
+
+sorted_summary = summary.sort_values(by='exp(coef)', ascending=False)
+sorted_summary
+
+#####################
+Risk Score
+
+# List of columns to drop
+columns_to_drop = ['time_to_event', 'Heat Stroke']
+
+# Create a new list with columns that are not in columns_to_drop
+cox_nonpca_features = [col for col in variables_cox_nonpca if col not in columns_to_drop]
+
+cox_nonpca_features
+
+df_cph_riskscore=df_cph[cox_nonpca_features]
+
+# Calculate predicted hazard ratios for each participant
+predicted_hazard_ratios = np.exp(np.dot(df_cph_riskscore.values, cph.params_))
+
+# Transform hazard ratios into risk scores (logarithmic transformation)
+risk_scores = np.log(predicted_hazard_ratios)
+
+# Scale risk scores to a specific range (e.g., 0-100)
+min_score = min(risk_scores)
+max_score = max(risk_scores)
+scaled_risk_scores = 100 * (risk_scores - min_score) / (max_score - min_score)
+
+#NEED TO EDIT THIS CATEGORY THING
+# Define risk categories based on risk scores
+def assign_risk_category(score):
+    if score < threshold_low:
+        return "Low Risk"
+    elif score < threshold_high:
+        return "Medium Risk"
+    else:
+        return "High Risk"
+
+# Set your desired threshold values for risk categories
+threshold_low = -1.0
+threshold_high = 1.0
+
+# Assign risk categories to participants
+risk_categories = [assign_risk_category(score) for score in scaled_risk_scores]
+
+# Add risk scores and categories to the original dataset (variables_cox_nonpca)
+df_cph_riskscore["Risk_Score"] = scaled_risk_scores
+df_cph_riskscore["Risk_Category"] = risk_categories
 
 #####################
 ######################## section-1 ##################
