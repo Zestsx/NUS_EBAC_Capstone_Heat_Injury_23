0f79d7f8dace3bf2478107ac680c47ad141ebe7c
Spencer <zestsx@gmail.com>
2023-08-25 20:24:12 +0800

    Update 3_🖥️_Model.py

--- pages/3_🖥️_Model.py
+++ pages/3_🖥️_Model.py
@@ -289,11 +289,10 @@ for column in df_rs.columns:
 
 result_df = pd.DataFrame.from_dict(result_dict, orient='index')
 
-#Left Join Result above
-sorted_summary['coef']
+sorted_summary.set_index(df.columns[0], inplace=True)
 
 # Perform the left join and merge based on a common key column
-merged_df = sorted_summary['coef'].merge(result_df, left_on=sorted_summary['coef'].index, result_df=True, how='left')
+merged_df = sorted_summary.merge(result_df, left_on=sorted_summary['coef'].index, result_df=True, how='left')
 
 # Display the result DataFrame
 merged_df
